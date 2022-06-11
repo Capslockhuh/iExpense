9834a4f7d0507a2a90006b0f5f8b0856c99f49d9
Capslockhuh <jas.andrzejewski2008@gmail.com>
2022-06-11 09:56:02 +0200

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Jan Andrzejewski on 11/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
