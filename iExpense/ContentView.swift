44ce93273d76fc4b145415c3be0ef2c1a46ee94d
Capslockhuh <jas.andrzejewski2008@gmail.com>
2022-06-12 12:00:30 +0200

    Added the functionality to add and remove items

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -9,22 +9,43 @@ import SwiftUI
 
 struct ContentView: View {
     @StateObject var expenses = Expenses()
+    
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
+    }
+    
     var body: some View {
         NavigationView {
             List {
                 ForEach(expenses.items, id: \.name) { item in
                     Text(item.name)
                 }
+                .onDelete(perform: removeItems)
             }
             .navigationTitle("iExpense")
+            .toolbar {
+                Button {
+                    let expense = ExpenseItem(name: "Test", type: "Personal", amount: 5)
+                    expenses.items.append(expense)
+                } label: {
+                    Image(systemName: "plus")
+                }
+            }
         }
     }
+}
+
+struct ExpenseItem {
+    var name: String
+    var type: String
+    var amount: Double
 }
 
 class Expenses: ObservableObject {
     @Published var items = [ExpenseItem]()
     
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
